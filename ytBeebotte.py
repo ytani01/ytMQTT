35457df2715da34c8706cdf54d57d8571460ffb6
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2019-11-18 04:28:39 +0900

    update

---
+++ ytBeebotte.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+#
+# (C) 2019 Yoichi Tanibayashi
+#
+__author__ = 'Yoichi Tanibayashi'
+__date__   = '2019'
+
+from ytMqtt import Mqtt
+import time
+
+from MyLogger import get_logger
+
+
+class Beebotte(Mqtt):
+    HOST = 'mqtt.beebotte.com'
+    PORT = 1883
+
+    DEF_QOS = 0
+
+    def __init__(self, topic, debug=False):
+        self._debug = debug
+        self._logger = get_logger(__class__.__name__, self._debug)
+        self._logger.debug('topic=%s', topic)
+
+        super().__init__(self.HOST, topic, port=self.PORT, debug=self._debug)
+
+    def publish(self, topic, data, qos=DEF_QOS, retain=False):
+        self._logger.debug('topic=%s, data=%s, qos=%d, retain=%s',
+                           topic, data, qos, retain)
+
+        ts = int(time.time() * 1000)
+        payload = {'data': data, 'ts': ts, 'ispublic': True}
+        self._logger.debug('ts=%d, payload=%s', ts, payload)
+
+        super().publish(topic, payload, qos=qos, retain=retain)
+
+
+class App:
+    def __init__(self, topic, debug=False):
+        self._debug = debug
+        self._logger = get_logger(__class__.__name__, self._debug)
+        self._logger.debug('topic=%s', topic)
+
+        self._topic = topic
+
+        self._bbt = Beebotte(self._topic, debug=self._debug)
+        self._bbt.start()
+
+    def main(self):
+        self._logger.debug('')
+
+        self._bbt.subscribe()
+        self._bbt.publish('env1/temp', 'hello')
+
+        for i in range(5):
+            msg_type, msg_data = self._bbt.msg_get()
+            if msg_type == Beebotte.MSG_DATA:
+                topic = msg_data['topic']
+                data = msg_data['data']
+                print('(%d) %s: %s' % (i, topic, data))
+            else:
+                print('(%d) %s' % (i, msg_data))
+            time.sleep(2)
+
+    def end(self):
+        self._logger.debug('')
+        self._bbt.end()
+
+
+import click
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.command(context_settings=CONTEXT_SETTINGS,
+               help='''
+beebotte MQTT class
+''')
+@click.argument('topic1')
+@click.argument('topic2', nargs=-1)
+@click.option('--debug', '-d', 'debug', is_flag=True, default=False,
+              help='debug flag')
+def main(topic1, topic2, debug):
+    logger = get_logger(__name__, debug=debug)
+
+    topic = [topic1] + list(topic2)
+
+    app = App(topic, debug=debug)
+    try:
+        app.main()
+    finally:
+        logger.info('finally')
+        app.end()
+
+
+if __name__ == '__main__':
+    main()
--- ytMqtt.py
+++ ytMqtt.py
@@ -6,14 +6,16 @@ __author__ = 'Yoichi Tanibayashi'
 __date__   = '2019'
 
 import paho.mqtt.client as mqtt
+import queue
 import os
 import csv
 import json
+import time
 
 from MyLogger import get_logger
 
 
-class MqttCommon:
+class Mqtt:
     CONF_FILENAME = ['mqtt.conf', '.mqtt.conf']
     CONF_PATH = ['.', os.environ['HOME'], '/etc']
 
@@ -21,10 +23,17 @@ class MqttCommon:
 
     DEF_QOS = 0
 
+    MSG_OK   = 0    # {'type': MSG_OK,   'data': 'message'}
+    MSG_DATA = 1    # {'type': MSG_DATA, 'data': {data}}
+    MSG_ERR  = 255  # {'type': MSG_ERR,  'data': 'mesage'}
+
     def __init__(self, host, topic, port=DEF_PORT, debug=False):
         self._debug = debug
         self._logger = get_logger(__class__.__name__, self._debug)
         self._logger.debug('host=%s:port=%d, topic=%s', host, port, topic)
+
+        if len(topic) == 0:
+            return
 
         self._svr_host = host
         self._svr_port = port
@@ -35,11 +44,15 @@ class MqttCommon:
         self._user = ''
         self._pw   = ''
 
+        self._msgq = queue.Queue()
+
         if self.load_conf() is None:
+            self._logger.error('load_conf(): failed')
             return
 
         ret = self.get_user_pw()
         if ret is None:
+            self._logger.error('get_user_pw(): failed (ret=%s)', ret)
             return
 
         self._user, self._pw = ret
@@ -54,6 +67,19 @@ class MqttCommon:
         self._mqttc.on_publish = self.on_publish
 
         self._mqttc.username_pw_set(self._user, self._pw)
+
+    def msg_put(self, msg_type, msg_data):
+        self._logger.debug('msg_type=%s, msg_data=%s', msg_type, msg_data)
+
+        msg = {'type': msg_type, 'data': msg_data}
+        self._msgq.put(msg)
+
+    def msg_get(self):
+        self._logger.debug('')
+
+        msg = self._msgq.get()
+        self._logger.debug('msg=%s', msg)
+        return msg['type'], msg['data']
 
     def connect(self, keepalive=60):
         self._logger.debug('')
@@ -78,7 +104,7 @@ class MqttCommon:
         if topic is not None:
             self._mqttc.subscribe(topic, qos=qos)
             return
-        
+
         for t in self._topic:
             self._mqttc.subscribe(t, qos=qos)
 
@@ -88,7 +114,7 @@ class MqttCommon:
         if topic is not None:
             self._mqttc.unsibscribe(topic)
             return
-        
+
         for t in self._topic:
             self._mqttc.unsubscribe(t)
 
@@ -108,24 +134,35 @@ class MqttCommon:
         self._logger.debug('userdata=%s, flag=%s, rc=%s', userdata, flag, rc)
 
         if rc != 0:
+            self.msg_put(self.MSG_ERR, 'connect error')
             return
+
+        self.msg_put(self.MSG_OK, 'connect ok')
 
     def on_disconnect(self, client, userdata, rc):
         self._logger.debug('userdata=%s, rc=%s', userdata, rc)
 
         if rc != 0:
+            self.msg_put(self.MSG_ERR, 'disconnect error')
             return
+
+        self.msg_put(self.MSG_OK, 'disconnect ok')
 
     def on_message(self, client, userdata, msg):
         self._logger.debug('userdata=%s', userdata)
 
         topic = msg.topic
+
         self._logger.debug('msg.payload=%s', msg.payload)
         payload = json.loads(msg.payload.decode('utf-8'))
         self._logger.debug('topic=%s, payload=%s', topic, payload)
 
+        msg_data = {'topic': topic, 'data': payload}
+        self.msg_put(self.MSG_DATA, msg_data)
+
     def on_publish(self, client, userdata, mid):
         self._logger.debug('userdata=%s, mid=%s', userdata, mid)
+        self.msg_put(self.MSG_OK, 'published(mid=%s)' % mid)
 
     def find_conf(self):
         self._logger.debug('')
@@ -186,15 +223,25 @@ class MqttCommon:
 
         self.connect()
         self.loop_start()
+        time.sleep(1)
+
+        """
+        msg_type, msg_data = self.msg_get()
+        self._logger.debug('%s: %s', msg_type, msg_data)
+        """
 
     def end(self):
         self._logger.debug('')
 
         self.disconnect()
+        msg_type, msg_data = self.msg_get()
+        self._logger.debug('%s: %s', msg_type, msg_data)
+
         self.loop_stop()
 
+
 class App:
-    def __init__(self, host, topic, port=MqttCommon.DEF_PORT, debug=False):
+    def __init__(self, host, topic, port=Mqtt.DEF_PORT, debug=False):
         self._debug = debug
         self._logger = get_logger(__class__.__name__, self._debug)
         self._logger.debug('host=%s:port=%d, topic=%s', host, port, topic)
@@ -203,12 +250,25 @@ class App:
         self._svr_port = port
         self._topic = topic
 
-        self._mqtt = MqttCommon(self._svr_host, self._topic, self._svr_port,
-                                debug=self._debug)
+        self._mqtt = Mqtt(self._svr_host, self._topic, self._svr_port,
+                          debug=self._debug)
         self._mqtt.start()
-        
+
     def main(self):
         self._logger.debug('')
+
+        self._mqtt.subscribe()
+        self._mqtt.publish('env1/temp', 'hello')
+
+        for i in range(5):
+            msg_type, msg_data = self._mqtt.msg_get()
+            if msg_type == Mqtt.MSG_DATA:
+                topic = msg_data['topic']
+                data = msg_data['data']
+                print('(%d) %s: %s' % (i, topic, data))
+            else:
+                print('(%d) %s' % (i, msg_data))
+            time.sleep(2)
 
     def end(self):
         self._logger.debug('')
@@ -227,7 +287,7 @@ MQTT Common class
 @click.argument('topic1')
 @click.argument('topic2', nargs=-1)
 @click.option('--server_port', '--port', '-p', 'server_port', type=int,
-              default=MqttCommon.DEF_PORT,
+              default=Mqtt.DEF_PORT,
               help='server port')
 @click.option('--debug', '-d', 'debug', is_flag=True, default=False,
               help='debug flag')
@@ -235,7 +295,7 @@ def main(server_host, server_port, topic1, topic2, debug):
     logger = get_logger(__name__, debug=debug)
 
     topic = [topic1] + list(topic2)
-    
+
     app = App(server_host, topic, server_port, debug=debug)
     try:
         app.main()
